2a1105aa331cd852b2579b62d90cb56f191557a7
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-03 22:19:15 +1000

    fixed some minor errors

--- server/channel_leave_test.py
+++ server/channel_leave_test.py
@@ -2,7 +2,7 @@ from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
 from f_channel_join import channel_join
-from f_channet_leave import channel_leave
+from f_channel_leave import channel_leave
 import pytest
 
 def test_channel_leave(): 
@@ -14,9 +14,9 @@ def test_channel_leave():
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-    authRegisterDic = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic['token']
-    u_id1 = authRegisterDic['u_id']
+    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token1 = authRegisterDic1['token']
+    u_id1 = authRegisterDic1['u_id']
     # SET UP END
     
     channel_join(token1, channel_id)
@@ -30,7 +30,7 @@ def test_channel_leave():
     assert channel_details(token, channel_id) = {"name": "validchannel", "owner_members": [{"u_id": u_id, "name_first": "firstname", "name_last": "lastname"}], "all_members": [{}]}
     
     
-def test_channel_join_bad(): 
+def test_channel_leave_bad(): 
     
     # SET UP BEGIN 
     authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
@@ -39,12 +39,34 @@ def test_channel_join_bad():
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-    authRegisterDic = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic['token']
-    u_id1 = authRegisterDic['u_id']
+    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token1 = authRegisterDic1['token']
+    u_id1 = authRegisterDic1['u_id']
     # SET UP END 
     
     channel_join(token1, channel_id)
     with pytest.raises(ValueError): 
-        channel_details(token1, "invalidchannel_id")
+        # calling function using invalid channel_id
+        channel_leave(token1, "invalidchannel_id")
+
+def test_channel_leave_bad(): 
     
+    # SET UP BEGIN 
+    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    token = authRegisterDic['token']
+    u_id = authRegisterDic['u_id']
+    channelsCreateDic = channels_create(token, "validchannel", True)
+    channel_id = channelsCreateDic['channel_id']
+    
+    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token1 = authRegisterDic1['token']
+    u_id1 = authRegisterDic1['u_id']
+    # SET UP END 
+    
+    channel_join(token1, channel_id)
+    auth_logout(token1)
+    with pytest.raises(ValueError): 
+        # calling function using invalid token
+        channel_leave(token1, channel_id)
+
+
