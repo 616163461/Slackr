22bad0e07f8766bae055d56142fd0b25a691a393
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-03 22:19:07 +1000

    fixed some minor errors

--- server/channel_join_test.py
+++ server/channel_join_test.py
@@ -19,7 +19,7 @@ def test_channel_join():
     # SET UP END 
     
     channel_join(token1, channel_id)
-    #assuming admin isn't in all_members list since admin was specifically isn't a member
+    # assuming admin isn't in all_members list since admin was specifically isn't a member
     assert channel_details(token, channel_id) == {"name": "validchannel", "owner_members": [{"u_id": u_id, "name_first": "firstname", "name_last": "lastname"}], "all_members": [{"u_id": u_id1, "name_first": "firstname1", "name_last": "lastname1"}]}
     
     
@@ -30,9 +30,9 @@ def test_channel_join_bad():
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     
-    authRegisterDic = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic['token']
-    u_id1 = authRegisterDic['u_id']
+    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token1 = authRegisterDic1['token']
+    u_id1 = authRegisterDic1['u_id']
     # SET UP END 
     
     with pytest.raises(ValueError): 
@@ -47,15 +47,22 @@ def test_channel_join_bad1():
     channelsCreateDic = channels_create(token, "validchannel", False)
     channel_id = channelsCreateDic['channel_id']
     
-    authRegisterDic = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic['token']
-    u_id1 = authRegisterDic['u_id']
+    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token1 = authRegisterDic1['token']
+    u_id1 = authRegisterDic1['u_id']
     # SET UP END 
     
+    # invalidate token
+    auth_logout(token)
+    
     with pytest.raises(AccessError): 
+        # calling function using a token which isn't admin
         channel_details(token1, channel_id)
-    
-    
+        
+        # calling function using a token which was invalidated 
+        channel_details(token, channel_id)
+        
+        
     
     
     
