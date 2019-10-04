d809f463b6f234b65ec55e057f396605c7b69175
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-04 19:07:21 +1000

    channels_create tests done

--- server/channels_create_test.py
+++ server/channels_create_test.py
@@ -4,28 +4,32 @@ import pytest
 
 def test_channels_create(): 
     
-    dictionary = auth_register("geoffrey.he777@gmail.com", "PigBabo", "Geoffrey", "He")
-    u_id = dictionary["u_id"]
-    token = dictionary["token"]
+    # SET UP BEGIN 
+    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    token = authRegisterDic['token']
+    u_id = authRegisterDic['u_id']
     
-    #assumed all returned channel_id are 12345
-    #case public good/study channel
-    assert channels_create(token, "study channel", True) == {"channel_id" : 1111}
-    #case private good/study channel
-    assert channels_create(token, "study channel", False) == {"channel_id" : 1111}
-    #case public bad/gaming channel
-    assert channels_create(token, "gaming channel", True) == {"channel_id" : 0000}
-    #case private bad/gaming channel
-    assert channels_create(token, "gaming channel", False) == {"channel_id" : 0000}
+    # SET UP END 
+    
+    assert channels_create(token, "validchannel", True) == {channel_id: "validchannel"}
+
     
 def test_channels_create_bad(): 
-    #case where name is too long
-    dictionary = auth_register("geoffrey.he777@gmail.com", "PigBabo", "Geoffrey", "He")
-    u_id = dictionary["u_id"]
-    token = dictionary["token"]
+   
+    # SET UP BEGIN 
+    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    token = authRegisterDic['token']
+    u_id = authRegisterDic['u_id']
+    
+    # SET UP END 
     
     with pytest.raises(ValueError): 
-        #case public channel
+        # calling function with public channel_name which is too long 
         channels_create(token, "this name is way too long so it will cause an error", True)
-        #case private channel
+        # calling function with private channel name which is too long
         channels_create(token, "this name is way too long so it will cause an error", False)
+        
+        
+        
+        
+        
