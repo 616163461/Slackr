a8236ef56720ef661748f0fe8e9f73ffbe5ddbc0
Matt <616163461@qq.com>
2019-10-06 07:22:19 +1100

    new

---
+++ server/f_auth_register.py
@@ -0,0 +1,15 @@
+# Function name: auth_register()
+# Parameters: (email, password, name_first, name_last)
+# Return type: { u_id, token }
+# Exception: ValueError when:
+# - Email entered is not a valid email.
+# - Email address is already being used by another user
+# - Password entered is not a valid password
+# - name_first is more than 50 characters
+# - name_last is more than 50 characters
+# Description: Given a user's first and last name, email address, and password, 
+# create a new account for them and return a new token for authentication in their session
+#
+
+def auth_register(email, password, name_first, name_last): 
+    pass
