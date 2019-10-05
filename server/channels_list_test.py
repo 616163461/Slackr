2dad3e2ffc34275172cc99e7d1564fd8bc1c0d03
Matt <616163461@qq.com>
2019-10-06 06:51:30 +1100

    new

---
+++ server/channels_list_test.py
@@ -0,0 +1,52 @@
+# Function name: channels_list()
+# Parameters: (token)
+# Return type: { channels }
+# Exception: N/A
+# Description: Provide a list of all channels (and their associated details) that the authorised user is part of
+#
+
+import pytest
+from f_channel_details import channel_details
+from f_auth_register import auth_register
+from f_channels_create import channels_create
+from f_channel_join import channel_join
+from f_channet_leave import channel_leave
+from f_channels_list import channels_list
+from f_auth_logout import auth_logout
+
+
+def test_channels_list(): 
+    
+    # SETUP BEGIN 
+    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    token = authRegisterDic['token']
+    u_id = authRegisterDic['u_id']
+    channelsCreateDic = channels_create(token, "validchannel", True)
+    channel_id = channelsCreateDic['channel_id']
+    
+    authRegisterDicOne = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
+    token_one = authRegisterDicOne['token']
+    u_id_one = authRegisterDicOne['u_id']
+    # SETUP END
+    
+    # Testing function using authorised user
+    assert channels_list(token) = [{channels: {channel_id: "validchannel"}}]
+    
+    
+def test_channels_list_bad(): 
+    
+    # SETUP BEGIN 
+    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    token = authRegisterDic['token']
+    u_id = authRegisterDic['u_id']
+    channelsCreateDic = channels_create(token, "validchannel", True)
+    channel_id = channelsCreateDic['channel_id']
+    
+    # SETUP END 
+    
+    
+    auth_logout(token)
+    with pytest.raises(ValueError):
+        # Testing function using invalid token 
+        channels_list(token)
+
