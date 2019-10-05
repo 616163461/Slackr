2b0dac8485bd3d72ed8b117843d27d1344694f21
Matt <616163461@qq.com>
2019-10-06 07:17:03 +1100

    new

---
+++ server/f_auth_passwordreset_reset.py
@@ -0,0 +1,14 @@
+# Function name: auth_passwordreset_reset()
+# Parameters: (reset_code, new_password)
+# Return type: {}
+# Exception: ValueError when:
+# - reset_code is not a valid reset code
+# - Password entered is not a valid password
+# Description: Given a reset code for a user, set that user's new password to the password provided
+#
+
+def auth_passwordreset_reset(reset_code, new_password):
+    
+    auth_passwordreset_reset_dict = {}
+
+    return auth_passwordreset_reset_dict
