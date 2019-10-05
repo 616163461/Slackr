bf911424cf01f50f3b28386f445bd827e9278a8a
Richard Jiang <z5207821@ad.unsw.edu.au>
2019-10-05 13:58:58 +1000

    Completed user_profile_sethandle.py

---
+++ server/f_user_profile_sethandle.py
@@ -0,0 +1,8 @@
+#Function name: user_profile_sethandle
+#Parameters: (token, handle_str)
+#Return value: {}
+#Value Error: handle_str is no more than 20 characters
+#Description: Update the authorised user's handle (i.e. display name)
+
+def user_profile_sethandle(tokne, handle_str):
+    pass
