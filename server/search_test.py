3c868eca2572c28d767e6706c1b1de369d47defd
Richard Jiang <z5207821@ad.unsw.edu.au>
2019-10-05 18:01:35 +1000

    Updated search_test.py

--- server/search_test.py
+++ server/search_test.py
@@ -11,8 +11,9 @@ from f_channels_create import channels_create
 from f_auth_register import auth_register
 from f_auth_logout import auth_logout
 from f_channel_invite import channel_invite
+from f_channel_messages import channel_messages
 
-def test_search():
+def test_search(): 
     
     #SET UP BEGIN
     #User One 
@@ -29,9 +30,15 @@ def test_search():
     invalid_token = invalidAuthRegisterDic['token']
     auth_logout(invalid_token) #Creates an Invalid Token
     
+    #Create a channel
     channel_id = channels_create(token, "Channel Nine", True)
+    
+    #Invite the members
     channel_invite(token_user_one, channel_id, u_id_one)
     channel_invite(token_user_two, channel_id, u_id_two)
+    
+    #Send the messages
+    message_send(token_user_two, channel_id, "Safe and secure society")
     message_send(token_user_one, channel_id, "Hello")
     message_send(token_user_two, channel_id, "Hellomydude")
     message_send(token_user_one, channel_id, "Hellomyman")
@@ -40,26 +47,35 @@ def test_search():
     message_send(token_user_one, channel_id, "Congratulations on the role!")
     message_send(token_user_two, channel_id, "Happy work anniversary!")
     message_send(token_user_one, channel_id, "New world. New skills.")
-    message_send(token_user_two, channel_id, "Safe and secure society")
     message_send(token_user_one, channel_id, "New world")
+    
+    #Find the Message ID's
+    channelMessagesDic = channel_messages(token, channel_id, 0)
+    message_list = channelMessagesDic["messages"]
+    message_dic = message_list[0]
+    message_id_safe = message_dic["message_id"]
+    message_dic_two = message_list[2]
+    message_id_two = message_dic_two["message_id"]
+    message_dic_three = message_list[3]
+    message_id_hellomyman = message_dic_three["message_id"]
     #SET UP END
     
     #Testing for no results
     assert search(token_user_one, "???") == {}
     
     #Testing for one result
-    assert search(token_user_one, "Safe") == {"message_id" : 111 , "u_id": u_id_two, "message" : "Safe and secure society", "time_created" : "19:35", "is_unread" : True}
+    assert search(token_user_one, "Safe") == {"message_id" : message_id_safe , "u_id": u_id_two, "message" : "Safe and secure society", "time_created" : "19:35", "is_unread" : True}
     
     #Testing for two or more results
-    assert search(token_user_one, "Hellomy") == [{  "message_id" : 112, 
-        "u_id": u_id_two, 
+    assert search(token_user_one, "Hellomy") == [{  "message_id" : 112,
+        "u_id": message_id_two, 
         "message" : "Hellomydude",
         "time_created" : "17:35",
         "is_unread" : True
-    } ,
+    },
     {
-       "message_id" : 113, 
-        "u_id": u_id_one, 
+       "message_id" : 113,
+        "u_id": message_id_three, 
         "message" : "Helloymyman",
         "time_created" : "18:35",
         "is_unread" : True
