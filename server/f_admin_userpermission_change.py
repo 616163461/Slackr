3b3da0357aa7a7bd7936f1457382c01b7ad48b87
Matt <616163461@qq.com>
2019-10-06 06:55:26 +1100

    new

---
+++ server/f_admin_userpermission_change.py
@@ -0,0 +1,16 @@
+# Function name: admin_userperm_change()
+# Parameters: (token, u_id, permission_id)
+# Return type: {}
+# Exception: ValueError when:
+# - u_id does not refer to a valid user
+# - permission_id does not refer to a value permission
+# AccessError when:
+# - The authorised user is not an admin or owner
+# Description: Given a User by their user ID, set their permissions to new permissions described by permission_id
+# 
+
+def admin_userperm_change(token, u_id, permission_id):        
+    
+    admin_userperm_change_dict = {}
+
+    return admin_userperm_change_dict
