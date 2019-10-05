2358596adfeb1067fccabaf4cf0ac99d37f6901a
Matt <616163461@qq.com>
2019-10-06 07:09:27 +1100

    new

---
+++ server/f_auth_passwordreset_request.py
@@ -0,0 +1,15 @@
+# Function name: auth_passwordreset_request()
+# Parameters: (email)
+# Return type: {}
+# Description: Given an email address, 
+# if the user is a registered user, 
+# send's them a an email containing a specific secret code, 
+# that when entered in auth_passwordreset_reset, 
+# shows that the user trying to reset the password is the one who got sent this email.
+#
+
+def auth_passwordreset_request(email):
+    
+    auth_passwordreset_request_dict = {}
+
+    return auth_passwordreset_request_dict
