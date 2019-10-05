030187a0336c3cb6f597122a77b299ec1966136d
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-05 12:58:35 +1000

    channel_details tests refined

--- server/channel_details_test.py
+++ server/channel_details_test.py
@@ -1,12 +1,13 @@
 from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
+from f_auth_logout import auth_logout
 import pytest
 
 def test_channel_details(): 
     
     # SET UP BEGIN 
-    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
@@ -20,37 +21,29 @@ def test_channel_details():
 def test_channel_details_bad(): 
     
     # SET UP BEGIN
-    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
-    token = authRegisterDic['token']
-    u_id = authRegisterDic['u_id']
-    
-    # SET UP END 
-    
-    with pytest.raises(ValueError): 
-        channel_details(token, "invalidchannel_id")
-        
-        
-        
-def test_channel_details_bad1(): 
-    
-    # SET UP BEGIN 
-    # creating user1 and channel1
-    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
+    authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-    # creating user2 and channel2
-    authRegisterDic1 = auth_register("validemail", "validpassword", "firstname", "lastname")
+    authRegisterDic1 = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token1 = authRegisterDic['token']
     u_id1 = authRegisterDic['u_id']
     channelsCreateDic1 = channels_create(token1, "validchannel1", True)
     channel_id1 = channelsCreateDic['channel_id']
-    
     # SET UP END 
-    with pytest.raises(AccessError): 
-        channel_details("invalidtoken", channel_id)
+    
+    with pytest.raises(ValueError): 
+        # calling function with invalid channel_id
+        channel_details(token, "invalidchannel_id")
+        # calling function with unauthorised user
         channel_details(token, channel_id1)
-        channel_details(token1, channel_id)
+
+        
+    auth_logout(token)
+    with pytest.raises(ValueError): 
+        # calling function with invalid token
+        channel_details(token, channel_id)
+        
         
