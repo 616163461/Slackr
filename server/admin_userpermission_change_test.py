691a8a835023b541c86d2e92b4e4ad4f617d95af
Daniel Yang <z5209680@unsw.edu.au>
2019-10-05 15:04:17 +1000

    Cleaned up test cases

--- server/admin_userpermission_change_test.py
+++ server/admin_userpermission_change_test.py
@@ -1,37 +1,40 @@
 import pytest
 from f_admin_userpermission_change import admin_userperm_change
 from f_auth_register import auth_register
+from f_auth_logout import auth_logout
 
 def test_admin_userperm_change():
+    
+    # SETUP START   
+    
+    registerValidUserDict = auth_register("thom_browne@gmail.com", "password", "Thom", "Browne")
+    token = registerValidUserDict["token"]
+    u_id = registerValidUserDict["u_id"]    
+    permission_id = 1    
+    
+    invalid_first_name = "thisnameiswaytoolongforafirstnamesoitreturnsvalueerror"
+    invalid_last_name = "thisnameiswaytoolongforalastnamesoitreturnsvalueerror"
+    
+    registerInvalidUserDict = auth_register("hwang_yeji@gmail.com", "feelspecial", "Hwang", "Yeji")
+    invalid_token = registerInvalidUserDict["token"]
+    invalid_uid = registerInvalidUserDict["u_id"]
+    invalid_permission_id = "a"
+    
+    auth_logout(invalid_token)
+    
+    # SETUP END
+    
+    # Asserting that default case works
+    assert admin_userperm_change(token, u_id, permission_id) == {}
+    
+    with pytest.raises(ValueError, match = r"*"):
    
-    registerValidDict = auth_register("thom_browne@gmail.com", "password", "Thom", "Browne")
-    valid_token = registerValidDict["token"]
-    valid_uid = registerValidDict["u_id"]    
-    valid_permission_id = 1
-    
-    assert admin_userperm_change(valid_token, valid_uid, valid_permission_id) == {}
-
-def test_bad_admin_userperm_change():
-
-    registerValidDict = auth_register("thom_browne@gmail.com", "password", "Thom", "Browne")
-    valid_token = registerValidDict["token"]
-    valid_uid = registerValidDict["u_id"]    
-    valid_permission_id = 1
-
-    
-    with pytest.raises(ValueError):
-        registerInvalidDict = auth_register("ihearttrimesters", "t", "asldakdhfakjhdfrkalfhrgakljdhgfalkhgfakldhgfkalhgfdlkjhgflkhagklsdgfahsdgfaljkhdgfajkldhsgfjkahdgfkajlgf", "asldakdhfakjhdfrkalfhrgakljdhgfalkhgfakldhgfkalhgfdlkjhgflkhagklsdgfahsdgfaljkhdgfajkldhsgfjkahdgfkajlgf")
-        invalid_token = registerInvalidDict["token"]
-        invalid_uid = registerInvalidDict["u_id"]
-        invalid_permission_id = "a"
         # invalid token
-        admin_userperm_change(invalid_token, valid_uid, valid_permission_id)
+        admin_userperm_change(invalid_token, u_id, permission_id)
         # invalid u_id
-        admin_userperm_change(valid_token, invalid_uid, valid_permission_id)
-        # invalid permission_id
-        admin_userperm_change(valid_token, valid_uid, invalid_permission_id)
+        admin_userperm_change(token, invalid_uid, permission_id)
         
     with pytest.raises(SystemError):
-        invalid_permid = 3
+    
         # invalid permission_id
-        admin_userperm_change(valid_token, valid_uid, invalid_permid)
+        admin_userperm_change(token, u_id, invalid_permission_id)
