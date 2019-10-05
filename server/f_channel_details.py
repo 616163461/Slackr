df90ac1a73817aeca53b37fd03a4febd8c1397b2
Matt <616163461@qq.com>
2019-10-06 07:12:35 +1100

    new

---
+++ server/f_channel_details.py
@@ -0,0 +1,12 @@
+# Function name: channel_details()
+# Parameters: (token, channel_id)
+# Return type: { name, owner_members, all_members }
+# Exception: ValueError when:
+# - Channel (based on ID) does not exist
+# AccessError when:
+# - Authorised user is not a member of channel with channel_id
+# Description: Given a Channel with ID channel_id that the authorised user is part of, provide basic details about the channel
+#
+
+def channel_details(token, channel_id): 
+    pass
