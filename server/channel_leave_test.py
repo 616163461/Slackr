a813be383ea39489c75e681ac0ed819d84913523
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-05 13:29:02 +1000

    channel_leave test refined

--- server/channel_leave_test.py
+++ server/channel_leave_test.py
@@ -43,28 +43,19 @@ def test_channel_leave_bad():
     authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
     token1 = authRegisterDic1['token']
     u_id1 = authRegisterDic1['u_id']
+    
+    authRegisterDic2 = auth_register("validemail2", "validpassword2", "firstname2", "lastname2")
+    token2 = authRegisterDic2['token']
+    u_id2 = authRegisterDic2['u_id']
     # SET UP END 
     
     channel_join(token1, channel_id)
     with pytest.raises(ValueError): 
         # calling function using invalid channel_id
         channel_leave(token1, "invalidchannel_id")
-
-def test_channel_leave_bad(): 
-    
-    # SET UP BEGIN 
-    authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
-    token = authRegisterDic['token']
-    u_id = authRegisterDic['u_id']
-    channelsCreateDic = channels_create(token, "validchannel", True)
-    channel_id = channelsCreateDic['channel_id']
-    
-    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
-    # SET UP END 
-    
-    channel_join(token1, channel_id)
+        # calling function on user who isn't part of the channel
+        channel_leave(token2, channel_id)
+        
     auth_logout(token1)
     with pytest.raises(ValueError): 
         # calling function using invalid token
