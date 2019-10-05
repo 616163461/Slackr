1d304a539031fa3f414b4c7b33fc54eceac0ce4f
Daniel Yang <z5209680@unsw.edu.au>
2019-10-05 16:56:40 +1000

    Finalised all test cases, made more changes to generating users

--- server/admin_userpermission_change_test.py
+++ server/admin_userpermission_change_test.py
@@ -1,19 +1,19 @@
-import pytest
 from f_admin_userpermission_change import admin_userperm_change
 from f_auth_register import auth_register
 from f_auth_logout import auth_logout
+import pytest
 
 def test_admin_userperm_change():
     
-    # SETUP START   
+    # SET UP BEGIN   
     
-    # Generate a valid user dictionary
+    # Generate a valid user
     registerValidUserDict = auth_register("thom_browne@gmail.com", "password", "Thom", "Browne")
     token = registerValidUserDict["token"]
     u_id = registerValidUserDict["u_id"]    
     permission_id = 1    
     
-    # Generate an invalid user dictionary
+    # Generate an invalid user
     registerInvalidUserDict = auth_register("hwang_yeji@gmail.com", "feelspecial", "Hwang", "Yeji")
     invalid_token = registerInvalidUserDict["token"]
     invalid_uid = registerInvalidUserDict["u_id"]
@@ -22,7 +22,7 @@ def test_admin_userperm_change():
     # Invalidate the invalid user
     auth_logout(invalid_token)
     
-    # SETUP END
+    # SET UP END
     
     # Asserting that the default case works
     assert admin_userperm_change(token, u_id, permission_id) == {}
@@ -30,14 +30,14 @@ def test_admin_userperm_change():
     # Testing that ValueError is raised when invalid parameters are passed
     with pytest.raises(ValueError, match = r"*"):
    
-        # invalid token
+        # calling function with an invalid token
         admin_userperm_change(invalid_token, u_id, permission_id)
         
-        # invalid u_id
+        # calling function with an invalid u_id
         admin_userperm_change(token, invalid_uid, permission_id)
         
     # Testing that SystemError is raised when invalid parameters are passed    
     with pytest.raises(SystemError):
     
-        # invalid permission_id
+        # calling function with an invalid permission_id
         admin_userperm_change(token, u_id, invalid_permission_id)
--- server/message_send_test.py
+++ server/message_send_test.py
@@ -1,30 +1,35 @@
-import pytest
 from f_message_send import message_send
 from f_auth_register import auth_register
 from f_channels_create import channels_create
+from f_channel_invite import channel_invite
+import pytest
 
 def test_message_send():
 
-    # SETUP START
+    # SET UP BEGIN
 
-    # Generate a valid user and a valid channel for "channel_id"
+    # Generate a valid user 
     registerValidUserDict = auth_register("hwangyeji@gmail.com", "feelspecial", "Hwang", "Yeji")
     token = registerValidUserDict["token"]
+    u_id = registerValidUserDict["u_id"]
     createValidChannelDict = channels_create(token, "validchannel", True)
     channel_id = createValidChannelDict["channel_id"]
+    channel_invite(token, channel_id, u_id)
     message = "I Heart Hwang Yeji"
 
-    # Generate an invalid user and an invalid "channel_id"
+    # Generate an invalid user
     registerInvalidUserDict = auth_register("kangdaniel@gmail.com", "password", "Kang", "Daniel")
     invalid_token = registerInvalidUserDict["token"]
+    invalid_uid = registerValidUserDict["u_id"]
     createInvalidChannelDict = channels_create(invalid_token, "invalidchannel", True)
     invalid_channelid = createInvalidChannelDict["channel_id"]
+    channel_invite(invalid_token, invalid_channelid, invalid_uid)
     invalid_message = 0
     
     # Invalidate the invalid user
     auth_logout(invalid_token)
     
-    # SETUP END 
+    # SET UP END
     
     # Asserting that the default case works
     assert message_send(token, channel_id, message) == {}
@@ -32,11 +37,11 @@ def test_message_send():
     # Testing that ValueError is raised when invalid parameters are passed
     with pytest.raises(ValueError, match = r"*"): 
 
-        # invalid token
+        # calling function with an invalid token
         message_send(invalid_token, channel_id, message)
         
-        # invalid channel id
+        # calling function with an invalid channel id
         message_send(token, invalid_channelid, message)
         
-        # invalid message
+        # calling function with an invalid message
         message_send(token, channel_id, invalid_message)
