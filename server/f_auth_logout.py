bbc2cafe9dccfa5cef7ea07cedcdc4045010da7d
Matt <616163461@qq.com>
2019-10-06 07:13:49 +1100

    new

---
+++ server/f_auth_logout.py
@@ -0,0 +1,12 @@
+# Function name: auth_logout()
+# Parameters: (token)
+# Return type: {}
+# Description: Given an active token, invalidates the taken to log the user out. 
+# Given a non-valid token, does nothing
+#
+
+def auth_logout(token):
+    
+    auth_logout_dict = {}
+
+    return auth_logout_dict
