9e22929b83f83709d59d27985df765be06de21f1
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:28:45 +1100

    Updated f_message_edit.py

--- server/f_message_edit.py
+++ server/f_message_edit.py
@@ -1,3 +1,11 @@
-def message_edit(token, message_id,message):
-    pass
+# Function name: message_edit()
+# Parameters: (token, message_id, message)
+# Return type: {}
+# Description: Given a message, update it's text with new text
+#
 
+def message_edit(token, message_id,message):
+    
+    message_edit_dict = {}
+    
+    return message_edit_dict
