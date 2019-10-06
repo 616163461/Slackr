fc37efc36919660b93a3e63c5a764f0ee0cc416d
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:38:05 +1100

    Updated f_channels_list.py

--- server/f_channels_list.py
+++ server/f_channels_list.py
@@ -1,6 +1,7 @@
 # Function name: channels_list()
 # Parameters: (token)
 # Return type: { channels }
+# Exception: N/A
 # Description: Provide a list of all channels (and their associated details) 
 # that the authorised user is part of
 #
