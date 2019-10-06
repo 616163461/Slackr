57507be7dcb8ff64d28ed716c6982cc755cbca1b
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:24:59 +1100

    Updated f_channel_details.py

--- server/f_channel_details.py
+++ server/f_channel_details.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Function name: channel_details()
 # Parameters: (token, channel_id)
 # Return type: { name, owner_members, all_members }
@@ -9,7 +8,5 @@
 # Description: Given a Channel with ID channel_id that the authorised user is part of, provide basic details about the channel
 #
 
-=======
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
 def channel_details(token, channel_id): 
     pass
