cb9e07ac961c61c71af115695e62237fc6d83e30
Daniel Yang <z5209680@unsw.edu.au>
2019-10-06 16:01:38 +1100

    Updated and finalised all functions

---
+++ server/f_message_pin.py
@@ -0,0 +1,17 @@
+# Function name: message_pin()
+# Parameters: (token, message_id)
+# Return type: {}
+# Exception: ValueError when:
+# - message_id is not a valid message
+# - The authorised user is not an admin
+# - Message with ID message_id is already pinned
+# AccessError when:
+# - The authorised user is not a member of the channel that the message is within
+# Description: Given a message within a channel, mark it as "pinned" to be given special display treatment by the frontend
+#
+
+def message_pin(token, message_id):
+    
+    message_pin_dict = {}
+
+    return message_pin_dict
