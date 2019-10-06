d28d702cc68d0f10e2ca553eeb9400459ffd341a
Geoffrey He <geoffrey.he@student.unsw.edu.au>
2019-10-06 17:33:30 +1100

    Update channel_messages_test.py

--- server/channel_messages_test.py
+++ server/channel_messages_test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Function name: channel_messages()
 # Parameters: (token, channel_id, start)
 # Return type: { messages, start, end }
@@ -16,8 +15,6 @@
 #
 
 import pytest
-=======
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
 from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
@@ -25,17 +22,10 @@ from f_channel_join import channel_join
 from f_channel_leave import channel_leave
 from f_auth_logout import auth_logout
 from f_message_send import message_send
-<<<<<<< HEAD
 
-
-def test_channel_messages(): 
-    # SETUP BEGIN 
-=======
-import pytest
 
 def test_channel_messages(): 
     # SET UP BEGIN 
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
     authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
@@ -47,7 +37,6 @@ def test_channel_messages():
     u_id1 = authRegisterDic1['u_id']
     
     message_send(token, channel_id, "validmessage")
-<<<<<<< HEAD
     # SETUP END
     
     # Couldn't assert since unable to obtain message_id also need to assert end == - 1
@@ -61,22 +50,6 @@ def test_channel_messages():
     channel_messages(token, channel_id, 0)
     
     # Checking that the start index works 
-=======
-    # SET UP END
-    
-    # couldn't assert since unable to obtain message_id also need to assert end == - 1
-    channel_messages(token, channel_id, 0)
-    
-    # adding 50 more messages to the channel to make 51 total messages
-    for i in range(0,50): 
-        message_send(token, channel_id, "validmessage")
-    
-    # should be asserting that end is not equal to -1      
-    channel_messages(token, channel_id, 0)
-    
-    # checking that the start index works 
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
-    channel_messages(token, channel_id, 31)
     
     
     
@@ -89,7 +62,6 @@ def test_channel_messages_bad():
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-<<<<<<< HEAD
     authRegisterDicOne = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
     token_one = authRegisterDicOne['token']
     u_id_one = authRegisterDicOne['u_id']
@@ -108,22 +80,3 @@ def test_channel_messages_bad():
         # Testing function with starting index greater than total number of messages in the channel
         channel_messages(token, channel_id, 10)
 
-=======
-    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
-    
-    message_send(token, channel_id, "validmessage")
-    # SET UP END
-    
-    with pytest.raises(ValueError): 
-        # calling function with invalid channel_id
-        channel_messages(token, "invalidchannel_id", 0)
-        # calling function with non-member of the channel
-        channel_messages(token1, channel_id, 0)
-       
-    message_send(token, channel_id, "secondvalidmessage")
-    with pytest.raises(ValueError): 
-        # calling function with starting index greater than total number of messages in the channel
-        channel_messages(token, channel_id, 10)
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
