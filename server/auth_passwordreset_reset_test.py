7a1737608fd30ce0eb551c4b11d0316e3a79b480
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:15:35 +1100

    Updated auth_passwordreset_reset_test.py

--- server/auth_passwordreset_reset_test.py
+++ server/auth_passwordreset_reset_test.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 # Function name: auth_passwordreset_reset()
 # Parameters:(reset_code, new_password)
 # Return type: {}
@@ -10,7 +8,6 @@
 #
 
 import pytest
->>>>>>> daniel_branch
 from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
@@ -20,32 +17,20 @@ from f_channels_list import channels_list
 from f_auth_logout import auth_logout
 from f_channel_addowner import channel_addowner
 from f_auth_passwordreset_reset import auth_passwordreset_reset
-<<<<<<< HEAD
-import pytest
-=======
 
->>>>>>> daniel_branch
 
 def test_auth_passwordreset_reset(): 
     
-    # SET UP BEGIN 
+    # SETUP BEGIN 
+    
     authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     
-    # SET UP END 
+    # SETUP END 
     
     auth_passwordreset_request("valid@email")
     
-<<<<<<< HEAD
-    # I don't have anyway of getting the reset_code so I can't test this
-    auth_passwordreset_reset("reset_code", "new_password")
-
-
-def test_auth_passwordreset_reset(): 
-    
-    # SET UP BEGIN 
-=======
     # setting password 
     auth_passwordreset_reset("reset_code", "new_password")
     
@@ -56,30 +41,17 @@ def test_auth_passwordreset_reset():
 def test_auth_passwordreset_reset_bad(): 
     
     # SETUP BEGIN 
->>>>>>> daniel_branch
+
     authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     
-<<<<<<< HEAD
-    # SET UP END 
-=======
     # SETUP END 
->>>>>>> daniel_branch
     
     auth_passwordreset_request("valid@email")
     
     with pytest.raises(ValueError):     
-<<<<<<< HEAD
-        # calling function with invalid reset code
-        auth_passwordreset_reset("invalid_reset_code", "new_password")
-        # calling function with invalid password
-        auth_passwordreset_reset("reset_code", "ivp")
-        
-=======
         # Testing function with invalid reset code
         auth_passwordreset_reset("invalid_reset_code", "new_password")
         # Testing function with invalid password
         auth_passwordreset_reset("reset_code", "ivp")
-
->>>>>>> daniel_branch
