ef2a7c963e2086fce9939453fdd3f41df8838fa3
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:22:30 +1100

    Updated f_auth_logout.py

--- server/f_auth_logout.py
+++ server/f_auth_logout.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Function name: auth_logout()
 # Parameters: (token)
 # Return type: {}
@@ -11,10 +10,3 @@ def auth_logout(token):
     auth_logout_dict = {}
 
     return auth_logout_dict
-=======
-def auth_logout(token): 
-    pass
-
-    
-    
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
