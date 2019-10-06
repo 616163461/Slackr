602eb3130787df054ff5a24d6d791b58c16a6e23
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-06 17:07:37 +1100

    Update user_profiles_uploadphoto_test.py

--- server/user_profiles_uploadphoto_test.py
+++ server/user_profiles_uploadphoto_test.py
@@ -1,8 +1,18 @@
+# Function: user_profiles_uploadphoto()
+# Parameters: (token, img_url, x_start, y_start, x_end, y_end)
+# Output: {}
+# Exception: ValueError when:
+# - img_url is returns an HTTP status other than 200.
+# - x_start, y_start, x_end, y_end are all within the dimensions of the image at the URL.
+# Description: Given a URL of an image on the internet, crops the image within bounds (x_start, y_start) 
+# and (x_end, y_end). Position (0,0) is the top left.
+#
+
+import pytest
+import requests
 from f_user_ profiles_uploadphoto import user_ profiles_uploadphoto
 from f_auth_register import auth_register
 from f_auth_logout import auth_logout
-import pytest
-import requests
 from PIL import Image
 from io import BytesIO
 
@@ -14,13 +24,13 @@ def user_ profiles_uploadphoto_test():
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
 
-    # bad picture
+    # Bad picture
     response1 = requests.get('img_url1') 
 	im1 = Image.open(BytesIO(response1.content))
 	x_start1 = 0
 	y_start1 = 0
 	x_end, y_end1, _ = im1.shape
-    # good picture but dimensions not correct
+    # Good picture but dimensions not correct
     response2 = requests.get('img_url2')
 	im2 = Image.open(BytesIO(response2.content))
 	x_start2 = 0
@@ -28,7 +38,7 @@ def user_ profiles_uploadphoto_test():
 	x_end2, y_end2, _ = im2.shape
 
 
-    #Testing successful run 
+    # Testing successful run 
     assert user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end) == {}
 
 
