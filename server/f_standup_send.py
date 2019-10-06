fbae0db7c3e60fa370dae4ffee4c3402937dd952
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:44:09 +1100

    Updated f_standup_send.py

--- server/f_standup_send.py
+++ server/f_standup_send.py
@@ -1,3 +1,12 @@
+# Function name: standup_send()
+# Parameters: (token, channel_id)
+# Return type: { time_finish }
+# Exception: ValueError when:
+# - Channel (based on ID) does not exist
+# AccessError when:
+# - The authorised user is not a member of the channel that the message is within
+# Description: Given a message within a channel the authorised user is part of, add a "react" to that particular message
+#
+
 def standup_start(token, channel_id, message):
     pass
-
