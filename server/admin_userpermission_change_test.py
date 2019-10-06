35d67967a106c3c7d62aae781161f5fb5e25d128
Richard Jiang <z5207821@ad.unsw.edu.au>
2019-10-06 16:33:48 +1100

    Updated admin_userpermisson_change.py

--- server/admin_userpermission_change_test.py
+++ server/admin_userpermission_change_test.py
@@ -1,11 +1,23 @@
+# Function: admin_userpermission_change()
+# Parameters: (token, u_id, permission_id)
+# Output: {}
+# Exception: ValueError when:
+# - u_id does not refer to a valid user
+# - permission_id does not refer to a value permission
+# AccessError when:
+# - The authorised user is not an admin or owner
+# Description: Given a User by their user ID, set their permissions to new permissions described by permission_id
+#
+
+import pytest
 from f_admin_userpermission_change import admin_userperm_change
 from f_auth_register import auth_register
 from f_auth_logout import auth_logout
-import pytest
+
 
 def test_admin_userperm_change():
     
-    # SET UP BEGIN   
+    # SETUP BEGIN   
     
     # Generate a valid user
     registerValidUserDict = auth_register("thom_browne@gmail.com", "password", "Thom", "Browne")
@@ -14,15 +26,15 @@ def test_admin_userperm_change():
     permission_id = 1    
     
     # Generate an invalid user
-    registerInvalidUserDict = auth_register("hwang_yeji@gmail.com", "feelspecial", "Hwang", "Yeji")
-    invalid_token = registerInvalidUserDict["token"]
-    invalid_uid = registerInvalidUserDict["u_id"]
+    registerInvalidUserDictTwo = auth_register("hwang_yeji@gmail.com", "feelspecial", "Hwang", "Yeji")
+    invalid_token = registerInvalidUserDictTwo["token"]
+    invalid_uid = registerInvalidUserDictTwo["u_id"]
     invalid_permission_id = "a"
     
     # Invalidate the invalid user
     auth_logout(invalid_token)
     
-    # SET UP END
+    # SETUP END
     
     # Asserting that the default case works
     assert admin_userperm_change(token, u_id, permission_id) == {}
@@ -30,14 +42,14 @@ def test_admin_userperm_change():
     # Testing that ValueError is raised when invalid parameters are passed
     with pytest.raises(ValueError, match = r"*"):
    
-        # calling function with an invalid token
+        # Testing function with an invalid token
         admin_userperm_change(invalid_token, u_id, permission_id)
         
-        # calling function with an invalid u_id
+        # Testing function with an invalid u_id
         admin_userperm_change(token, invalid_uid, permission_id)
         
     # Testing that SystemError is raised when invalid parameters are passed    
     with pytest.raises(SystemError):
     
-        # calling function with an invalid permission_id
+        # Testing function with an invalid permission_id
         admin_userperm_change(token, u_id, invalid_permission_id)
--- server/f_admin_userpermission_change.py
+++ server/f_admin_userpermission_change.py
@@ -1,2 +1,13 @@
+# Function: admin_userpermission_change()
+# Parameters: (token, u_id, permission_id)
+# Output: {}
+# Exception: ValueError when:
+# - u_id does not refer to a valid user
+# - permission_id does not refer to a value permission
+# AccessError when:
+# - The authorised user is not an admin or owner
+# Description: Given a User by their user ID, set their permissions to new permissions described by permission_id
+#
+
 def admin_userperm_change(token, u_id, permission_id):
     pass
