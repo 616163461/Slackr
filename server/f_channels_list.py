ad4430e108820c167b12824948a38249b839f7ed
Richard Jiang <z5207821@ad.unsw.edu.au>
2019-10-06 15:15:48 +1100

    Updated style on the functions

--- server/f_channels_list.py
+++ server/f_channels_list.py
@@ -1,2 +1,9 @@
+# Function name: channels_list()
+# Parameters: (token)
+# Return type: { channels }
+# Description: Provide a list of all channels (and their associated details) 
+# that the authorised user is part of
+#
+
 def channels_list(token): 
     pass
