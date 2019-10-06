4589569fb244148ecb5bcf30b15d2fd2eab268ea
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-06 17:27:31 +1100

    Update message_unreact_test.py

--- server/message_unreact_test.py
+++ server/message_unreact_test.py
@@ -1,3 +1,13 @@
+# Function: message_unreact()
+# Parameters: {token, message_id, react_id}
+# Output: {}
+# Exception: ValueError when:
+# - message_id is not a valid message within a channel that the authorised user has joined
+# - react_id is not a valid React ID
+# - Message with ID message_id does not contain an active React with ID react_id
+# Description: Given a message within a channel the authorised user is part of, remove a "react" to that particular message
+#
+
 import pytest
 from f_message_send import message_send
 from f_auth_register import auth_register
@@ -7,17 +17,16 @@ from f_message_unreact import message_unreact
 from f_message_react import message_react
 
 def test_message_unreact(): 
-    
-    # SET UP BEGIN 
+    # SETUP BEGIN 
     authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-    authRegisterDic1 = auth_register("valid@email1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
+    authRegisterDicOne = auth_register("valid@email1", "validpassword1", "firstname1", "lastname1")
+    token_one = authRegisterDicOne['token']
+    u_id_one = authRegisterDicOne['u_id']
     
     message_send(token, channel_id, "validmessage")
     channelMessagesDic = channel_messages(token, channel_id, 0)
@@ -25,9 +34,9 @@ def test_message_unreact():
     message_dic = message_list[0]
     message_id = message_dic["message_id"]
     
-    # assuming 123 is valid react_id
+    # Assuming 123 is valid react_id
     react_id = 123
-    # SET UP END
+    # SETUP END
     
     message_react(token, message_id, react_id)
     
@@ -35,53 +44,53 @@ def test_message_unreact():
         message_react(token, message_id, react_id)
         
     assert message_unreact(token, message_id, react_id) == {}
-    # calling message_react to check the message was successfully unreacted 
+    # Testing message_react to check the message was successfully unreacted 
     message_react(token, message_id, react_id)
     
-        
+    
 def test_message_unreact_bad(): 
     
-    # SET UP BEGIN 
+    # SETUP BEGIN
     authRegisterDic = auth_register("valid@email", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-    authRegisterDic1 = auth_register("valid@email1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
-    channel_join(token1, channel_id)
+    authRegisterDicOne = auth_register("valid@email1", "validpassword1", "firstname1", "lastname1")
+    token_one = authRegisterDicOne['token']
+    u_id_one = authRegisterDicOne['u_id']
+    channel_join(token_one, channel_id)
     
-    authRegisterDic2 = auth_register("valid@email2", "validpassword2", "firstname2", "lastname2")
-    token2 = authRegisterDic2['token']
-    u_id2 = authRegisterDic2['u_id']
+    authRegisterDicTwo = auth_register("valid@email2", "validpassword2", "firstname2", "lastname2")
+    token_two = authRegisterDicTwo['token']
+    u_id_two = authRegisterDicTwo['u_id']
     
     message_send(token, channel_id, "validmessage")
     channelMessagesDic = channel_messages(token, channel_id, 0)
     message_list = channelMessagesDic["messages"]
     message_dic = message_list[0]
     message_id = message_dic["message_id"]
-    # SET UP END
+    # SETUP END
 
     message_react(token, message_id, react_id)
     with pytest.raises(ValueError): 
-        # calling function with user who isn't admin 
-        message_unreact(token1, message_id, react_id)
-        # calling function with invalid message_id
+        # Testing function with user who isn't admin 
+        message_unreact(token_one, message_id, react_id)
+        # Testing function with invalid message_id
         message_unreact(token, "invalidmessage_id", react_id)
-        # calling function with user who isn't part of the channel
-        message_unreact(token2, message_id, react_id)
-        # calling function with invalid react_id
+        # Testing function with user who isn't part of the channel
+        message_unreact(token_two, message_id, react_id)
+        # Testing function with invalid react_id
         message_unreact(token, message_id, "invalidreact_id")
         
     message_unreact(token, message_id, react_id)
     with pytest.raises(ValueError):        
-        # calling function with already unreacted message_id
+        # Testing function with already unreacted message_id
         message_unreact(token, message_id, react_id)
         
     message_react(token, message_id, react_id)
     auth_logout(token)
     with pytest.raises(ValueError): 
-        # calling function with invalid token 
+        # Testing function with invalid token 
         message_unreact(token, message_id, react_id)
