f4d75ca83d0f6e60409fa703ea7312f89346dc4e
Daniel Yang <daniel.yang1@student.unsw.edu.au>
2019-10-06 17:34:54 +1100

    Updated f_message_sendlater.py

--- server/f_message_sendlater.py
+++ server/f_message_sendlater.py
@@ -1,2 +1,15 @@
+# Function name: message_sendlater()
+# Parameters: (token, channel_id, message, time_sent)
+# Return type: {}
+# Exception: ValueError when:
+# - Channel (based on ID) does not exist
+# - Message is more than 1000 characters
+# - Time sent is a time in the past
+# Description: Send a message from authorised_user to the channel specified by channel_id automatically at a specified time in the future
+#
+
 def send_message_later(token, channel_id, message, time_sent):
-    pass
+    
+    send_message_later_dict = {}
+    
+    return send_message_later_dict
