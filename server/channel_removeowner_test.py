c281c690790de6f5af48518741956e41faf4c408
Geoffrey He <geoffrey.he@student.unsw.edu.au>
2019-10-06 17:34:42 +1100

    Update channel_removeowner_test.py

--- server/channel_removeowner_test.py
+++ server/channel_removeowner_test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Function name: channel_removeowner()
 # Parameters: (token, channel_id, u_id)
 # Return type: {}
@@ -11,8 +10,6 @@
 #
 
 import pytest
-=======
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
 from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
@@ -22,26 +19,17 @@ from f_channels_list import channels_list
 from f_auth_logout import auth_logout
 from f_channel_removeowner import channel_removeowner
 from f_channel_addowner import channel_addowner
-<<<<<<< HEAD
 
-
-def test_channel_removeowner():
-    
-    # SETUP BEGIN
-=======
-import pytest
 
 def test_channel_removeowner():
     
     # SET UP BEGIN
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
     authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-<<<<<<< HEAD
     authRegisterDicOne = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
     token_one = authRegisterDicOne['token']
     u_id_one = authRegisterDicOne['u_id']
@@ -58,35 +46,15 @@ def test_channel_removeowner():
     # Calling function with recently removed owner to check he's not an owner
     channel_addowner(token_one, channel_id, u_id_two)
     
-def test_channel_removeowner_bad():
-    # SETUP BEGIN
-=======
-    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
-    
-    authRegisterDic2 = auth_register("validemail2", "validpassword2", "firstname2", "lastname2")
-    token2 = authRegisterDic2['token']
-    u_id2 = authRegisterDic2['u_id']
-    # SET UP END
-    
-    # adding user1 as an owner
-    channel_addowner(token, channel_id, u_id1)
-    # removing user1 as an owner
-    channel_removeowner(token, channel_id, u_id1)
-    # calling function with recently removed owner to check he's not an owner
-    channel_addowner(token1, channel_id, u_id2)
-    
+
 def test_channel_removeowner_bad():
     # SET UP BEGIN
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
     authRegisterDic = auth_register("validemail", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
     channelsCreateDic = channels_create(token, "validchannel", True)
     channel_id = channelsCreateDic['channel_id']
     
-<<<<<<< HEAD
     authRegisterDicOne = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
     token_one = authRegisterDicOne['token']
     u_id_one = authRegisterDicOne['u_id']
@@ -115,44 +83,3 @@ def test_channel_removeowner_bad():
         # Testing function with invalid token
         channel_removeowner(token, channel_id, u_id_one)
 
-=======
-    authRegisterDic1 = auth_register("validemail1", "validpassword1", "firstname1", "lastname1")
-    token1 = authRegisterDic1['token']
-    u_id1 = authRegisterDic1['u_id']
-    
-    authRegisterDic2 = auth_register("validemail2", "validpassword2", "firstname2", "lastname2")
-    token2 = authRegisterDic2['token']
-    u_id2 = authRegisterDic2['u_id']
-    
-    authRegisterDic3 = auth_register("validemail3", "validpassword3", "firstname3", "lastname3")
-    token3 = authRegisterDic3['token']
-    u_id3 = authRegisterDic3['u_id']
-    # SET UP END
-    channel_addowner(token, channel_id, u_id1)
-    
-    with pytest.raises(ValueError): 
-        # calling function with invalid channel_id
-        channel_removeowner(token, "invalidchannel_id", u_id1)
-        # calling function removing a user who isn't an owner
-        channel_removeowner(token, channel_id, u_id2)
-        # calling function with a user who isn't an owner
-        channel_removeowner(token2, channel_id, u_id3)
-        
-    auth_logout(token)
-    with pytest.raises(ValueError): 
-        # calling function with invalid token
-        channel_removeowner(token, channel_id, u_id1)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
->>>>>>> 35d67967a106c3c7d62aae781161f5fb5e25d128
