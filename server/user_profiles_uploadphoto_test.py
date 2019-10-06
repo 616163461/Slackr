08d84fb9b9386997ed0d0f298d25b7982acfe7b2
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-06 19:16:18 +1100

    Update user_profiles_uploadphoto_test.py

--- server/user_profiles_uploadphoto_test.py
+++ server/user_profiles_uploadphoto_test.py
@@ -18,32 +18,29 @@ from io import BytesIO
 
 
 def user_profiles_uploadphoto_test():
-
-
-	validAuthRegisterDic = auth_register("richard123@gmail.com", "validpassword", "firstname", "lastname")
+    validAuthRegisterDic = auth_register("richard123@gmail.com", "validpassword", "firstname", "lastname")
     token = authRegisterDic['token']
     u_id = authRegisterDic['u_id']
 
     # Bad picture
     response1 = requests.get('img_url1') 
-	im1 = Image.open(BytesIO(response1.content))
-	x_start1 = 0
-	y_start1 = 0
-	x_end, y_end1, _ = im1.shape
+    im1 = Image.open(BytesIO(response1.content))
+    x_start1 = 0
+    y_start1 = 0
+    x_end, y_end1, _ = im1.shape
     # Good picture but dimensions not correct
     response2 = requests.get('img_url2')
-	im2 = Image.open(BytesIO(response2.content))
-	x_start2 = 0
-	y_start2 = 0
-	x_end2, y_end2, _ = im2.shape
+    im2 = Image.open(BytesIO(response2.content))
+    x_start2 = 0
+    y_start2 = 0
+    x_end2, y_end2, _ = im2.shape
 
 
     # Testing successful run 
     assert user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end) == {}
 
-
-	with pytest.raises(ValueError):
-		# img_url returns an HTTP status other than 200
+    with pytest.raises(ValueError):
+        # img_url returns an HTTP status other than 200
         user_profiles_uploadphoto(token, response1.status_code, x_start1, y_start1, x_end1, y_end1)
         # x_start, y_start, x_end, y_end are all within the dimensions of the image at the URL
         user_profiles_uploadphoto(token, response2.status_code, x_start2, y_start2, x_end2, y_end2)
