1fbb37110186d073b782ebc3ffa5a7da164410af
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-20 17:43:15 +1100

    finished all the auth password reset functions

--- server/f_auth_passwordreset_request.py
+++ server/f_auth_passwordreset_request.py
@@ -18,10 +18,10 @@ from json import dumps
 APP = Flask(__name__)
 
 data = {
-    'users' : [ {'token' : '123456', 'handle_str' : 'richardjiang', 'first_name' : 'Richard', 'last_name' : 'Jiang', 'password' : 'hello12345', 'email' : 'richard@email.com', 'u_id' : '22222'} , 
-                {'token' : '654321', 'handle_str' : 'danielyang', 'first_name' : 'Daniel', 'last_name' : 'Yang', 'password' : 'mixedsignals', 'email' : 'dy@email.com', 'u_id' : '11111'},
-                {'token' : '678910', 'handle_str' : 'geoffreyhe', 'first_name' : 'Geoffrey', 'last_name' : 'He', 'password' : 'sohyun ', 'email' : 'geoffrey@email.com', 'u_id' : '77777'},
-                {'token' : '000000', 'handle_str' : 'mattma', 'first_name' : 'Matt', 'last_name' : 'Ma', 'password' : 'Thom&Jerry', 'email' : 'Matt@email.com', 'u_id' : '00000'}
+    'users' : [ {'token' : '123456', 'handle_str' : 'richardjiang', 'first_name' : 'Richard', 'last_name' : 'Jiang', 'password' : 'hello12345', 'email' : 'richard@email.com', 'u_id' : '22222', 'reset_code' : None} , 
+                {'token' : '654321', 'handle_str' : 'danielyang', 'first_name' : 'Daniel', 'last_name' : 'Yang', 'password' : 'mixedsignals', 'email' : 'dy@email.com', 'u_id' : '11111', 'reset_code' : None},
+                {'token' : '678910', 'handle_str' : 'geoffreyhe', 'first_name' : 'Geoffrey', 'last_name' : 'He', 'password' : 'sohyun ', 'email' : 'geoffrey@email.com', 'u_id' : '77777', 'reset_code' : None},
+                {'token' : '000000', 'handle_str' : 'mattma', 'first_name' : 'Matt', 'last_name' : 'Ma', 'password' : 'Thom&Jerry', 'email' : 'Matt@email.com', 'u_id' : '00000', 'reset_code' : None}
               ]
 }
 
@@ -52,23 +52,27 @@ def auth_passwordreset_request():
     mail = Mail(APP)
     data = getData()
     email = request.form.get('email')
-    
+    mask = 0
     user_list = data['users']
     for user in user_list:
         if user['email'] == email:
+            mask = 1
             first_name = user['first_name']
             last_name = user['last_name']
+            # assuming that reset_code is handle_str
+            reset_code = user['handle_str']
             try:
                 msg = Message("Reset Slackr password",
                     sender="snackathon123@gmail.com",
                     recipients=["snackathon123@gmail.com"])
-                msg.body = "Hey " + first_name + " " + last_name + "\n\n" + "Here's the special code to reset the password for your Slackr account.  \nSpecial code: CIMICTOP10" + "\n\n" + "Kind Regards, \nSlackr Support Team"
+                msg.body = "Hey " + first_name + " " + last_name + "\n\n" + "Here's the special code to reset the password for your Slackr account.  \nSpecial code: " + reset_code + "\n\n" + "Kind Regards, \nSlackr Support Team"
                 mail.send(msg)
+                user['reset_code'] = reset_code
                 return 'Slackr password reset code sent successfully!'
             except Exception as e:
                 return (str(e))
             
-    else:
+    if mask == 0:
         # raise error saying email doesn't exist in database
         return sendError("User's email doesn't exist in the database, please try again.")
         
