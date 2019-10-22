322f5ca3f8bab5df3f43d5aa46fb89c9365aa3d3
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-22 20:13:23 +1100

    added in data

--- server/f_user_profile_sethandle.py
+++ server/f_user_profile_sethandle.py
@@ -5,9 +5,61 @@
 # - handle_str is no more than 20 characters
 # Description: Update the authorised user's handle (i.e. display name)
 #
+from flask import Flask, request
+import json
 
-def user_profile_sethandle(token, handle_str):
+APP = Flask(__name__)
+
+data = {
+    'users' : [ {'token' : '123456', 'handle_str' : 'richardjiang', 'first_name' : 'Richard', 'last_name' : 'Jiang', 'password' : 'hash(hello12345)', 'email' : 'richard@email.com'} , {'token' : '654321', 'handle_str' : 'danielyang', 'first_name' : 'Daniel', 'last_name' : 'Yang', 'password' : 'mixedsignals', 'email' : 'dy@email.com'}]
+}
+
+def getData():
+    with open('export.json', 'r') as FILE:
+        data = json.load(FILE)
+    return data
+
+def sendSuccess(data):
+    return json.dumps(data)
     
-    user_profile_sethandle_dict = {}
+def updateData(data):
+    with open('export.json', 'w') as FILE:
+        json.dump(data, FILE)
+    return 0
+
+@APP.route('/user/profile/sethandle', methods = ['PUT'])    
+def user_profile_sethandle():
+
+    token = request.form.get('token')
+    handle_str = request.form.get('handle_str')
     
-    return user_profile_sethandle_dict
+    if len(handle_str) >= 20:
+        raise ValueError("Error: String is longer than 20 characters")
+        
+    flag = 0
+    data_new = getData() 
+    for i in data_new['users']:
+        if i['token'] == token and token != None:
+            i['handle_str'] = handle_str
+            flag = 1
+            answer = {}
+            updateData(data_new)
+            return sendSuccess(answer)
+    
+    if flag == 0:
+        print("Session has expired. Please refresh token\n")
+        return sendSuccess("Error")
+    else:
+        print("Successfully changed personal details\n")
+
+
+@APP.route('/names', methods = ['GET'])    
+def getnames():
+    return sendSuccess(data)
+
+
+
+if __name__ == '__main__':
+    APP.run(port = 7878)
+    
+
