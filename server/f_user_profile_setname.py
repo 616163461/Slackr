cbe052f389249805cdd026684cd431aea4c5051f
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-22 20:17:06 +1100

    data added

--- server/f_user_profile_setname.py
+++ server/f_user_profile_setname.py
@@ -7,8 +7,66 @@
 # Description: Update the authorised user's first and last name
 #
 
-def user_profile_setname(token, name_first, name_last):
+from flask import Flask, request
+import json
+
+#from server import getData
+'''
+The data global variable
+'''
+APP = Flask(__name__)
     
-    user_profile_setname_dict = {}
+data = {
+    'users' : [ {'token' : '123456', 'handle_str' : 'richardjiang', 'first_name' : 'Richard', 'last_name' : 'Jiang', 'password' : 'hash(hello12345)', 'email' : 'richard@email.com'} , {'token' : '654321', 'handle_str' : 'danielyang', 'first_name' : 'Daniel', 'last_name' : 'Yang', 'password' : 'mixedsignals', 'email' : 'dy@email.com'}]
+}
+
+
+def getData():
+    with open('export.json', 'r') as FILE:
+        data = json.load(FILE)
+    return data
     
-    return user_profile_setname_dict
+def sendSuccess(data):
+    return json.dumps(data)
+
+def updateData(data):
+    with open('export.json', 'w') as FILE:
+        json.dump(data, FILE)
+    return 0
+    
+    
+@APP.route('/user/profile/setname', methods=['PUT'])
+def user_profile_setname():
+    
+    token = request.form.get('token')
+    name_first = request.form.get('name_first')
+    name_last = request.form.get('name_last')
+    
+    if len(name_first) > 50 or len(name_last) > 50:
+        raise ValueError
+    flag = 0
+    data_new = getData() 
+    for i in data_new['users']:
+        if i['token'] == token:
+            i['first_name'] = name_first
+            i['last_name'] = name_last
+            flag = 1
+            answer = {}
+            updateData(data_new)
+            return sendSuccess(answer)
+    
+    if flag == 0:
+        print("Session has expired. Please refresh token\n")
+        raise ValueError
+    else:
+        print("Successfully changed personal details\n")
+
+        
+@APP.route('/names', methods = ['GET'])    
+def getnames():
+    return sendSuccess(data)
+
+if __name__ == '__main__':
+    APP.run(port = 7878)
+    
+
