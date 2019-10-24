2cb49af2c69b8c71a3e77d3fcbd6b63c7768da64
Geoffrey He <geoffrey.he777@gmail.com>
2019-10-24 22:12:11 +1100

    R

--- server/f_channels_list.py
+++ server/f_channels_list.py
@@ -1,10 +1,52 @@
 # Function name: channels_list()
 # Parameters: (token)
 # Return type: { channels }
-# Exception: N/A
 # Description: Provide a list of all channels (and their associated details) 
 # that the authorised user is part of
 #
+from flask import Flask, request
+import json
 
-def channels_list(token): 
-    pass
+APP = Flask(__name__)
+
+def getData():
+    with open('export.json', 'r') as FILE:
+        data = json.load(FILE)
+    return data
+
+# converting dictionary into string for flask
+def sendSuccess(data):
+    return json.dumps(data)
+
+@APP.route('/channel/list', methods=['GET'])
+def channels_list():
+    token = request.args.get('token')
+    data_new = getData()
+    
+    flag = 0
+    #Test for valid token
+    for i in data_new['users']:
+        if i['token'] == token and token != None:
+            u_id = i['u_id']
+            flag = 1
+    if flag == 0:
+        raise ValueError("Session has expired. Please refresh the page and login\n")
+    
+    answer = []
+    for channels in data_new['channels']:
+        valid = 0
+        for members in channels['all_members']:
+            if members['u_id'] == u_id:
+                valid = 1
+        if valid == 1:
+            temp = {}
+            temp['name'] = channels['channel_name']
+            temp['channel_id'] = channels['channel_id']
+            answer.append(temp)
+   
+    return sendSuccess(answer)
+      
+
+if __name__ == '__main__':
+    APP.run(port = 7878)
+
