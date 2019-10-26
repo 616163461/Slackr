d3705f7b3b2e4e30811fcfd8d0ad34561d5ffa15
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-27 01:50:28 +1100

    Update f_channel_removeowner.py

--- server/f_channel_removeowner.py
+++ server/f_channel_removeowner.py
@@ -1,18 +1,16 @@
-# Function name: channel_removeowner()
-# Parameters: (token, channel_id, u_id)
-# Return type: {}
-# Exception: ValueError when:
-# - Channel (based on ID) does not exist
-# - When user with user id u_id is not an owner of the channel
-# AccessError when:
-# - the authorised user is not an owner of the slackr, or an owner of this channel
-# Description: Remove user with user id u_id an owner of this channel
-#
-
-from flask import Flask, request
+'''
+Function name: channel_removeowner()
+Parameters: (token, channel_id, u_id)
+Return type: {}
+Exception: ValueError when:
+- Channel (based on ID) does not exist
+- When user with user id u_id is not an owner of the channel
+AccessError when:
+- the authorised user is not an owner of the slackr, or an owner of this channel
+Description: Remove user with user id u_id an owner of this channel
+'''
 import json
-
-APP = Flask(__name__)
+import myexcept
 
 def getData():
     with open('export.json', 'r') as FILE:
@@ -22,19 +20,15 @@ def getData():
 # converting dictionary into string for flask
 def sendSuccess(data):
     return json.dumps(data)
-    
+
 def updateData(data):
     with open('export.json', 'w') as FILE:
         json.dump(data, FILE)
     return 0
 
-@APP.route('/channel/removeowner', methods = ['POST'])
-def channelRemoveOwner(): 
+def channel_removeowner(token, channel_id, u_id):
 
-    data = getData()    
-    token = request.form.get('token')
-    channel_id = request.form.get('channel_id')
-    u_id = request.form.get('u_id')
+    data = getData()
     found_Token = False
     found_User = False
     # finding token and user data
@@ -46,46 +40,40 @@ def channelRemoveOwner():
             name_first = user['first_name']
             name_last = user['last_name']
             found_User = True
-            
-    
+
+
+    if found_Token == False:
+        myexcept.token_error()
     if found_User == False:
-        raise ValueError("u_id not found in system.")
-    if found_Token == False: 
-        raise ValueError("token not found in system.")
-        
+        myexcept.invalid_user()
+
     found_Token = False
     found_User = False
     # checking user and token belongs to an owner
     for channel in data['channels']:
-        if str(channel['channel_id']) == channel_id: 
+        if str(channel['channel_id']) == channel_id:
             for owner in channel['owner_members']:
                 if owner['u_id'] == tu_id:
                     found_Token = True
                 if str(owner['u_id']) == u_id:
                     found_User = True
-    
-    if found_Token == False: 
+
+    if found_Token == False:
         raise ValueError("token does not belong to an owner.")
     if found_User == False:
         raise ValueError("u_id does not belong to an owner.")
- 
+
     send_Success = False
     # remove user from owner_members list
     for channel in data['channels']:
-        if str(channel['channel_id']) == channel_id: 
+        if str(channel['channel_id']) == channel_id:
             channel['owner_members'].remove({'u_id' : u_id,
-                'name_first' : name_first,
-                'name_last' : name_last
-            })
+                                             'name_first' : name_first,
+                                             'name_last' : name_last
+                                            })
             send_Success = True
             updateData(data)
             return sendSuccess({})
-    
-    if send_Success == False: 
-        raise ValueError("channel_id not found in system.")
-            
 
-        
-if __name__ == '__main__':
-    APP.run(port = 7878)
-
+    if send_Success == False:
+        myexcept.channel_not_found()
