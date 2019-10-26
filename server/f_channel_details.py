5ba38d183d3158fee663f8bf6514725c7719f8f2
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-27 01:44:01 +1100

    Update f_channel_details.py

--- server/f_channel_details.py
+++ server/f_channel_details.py
@@ -1,17 +1,17 @@
-# Function name: channel_details()
-# Parameters: (token, channel_id)
-# Return type: { name, owner_members, all_members }
-# Exception: ValueError when:
-# - Channel (based on ID) does not exist
-# AccessError when:
-# - Authorised user is not a member of channel with channel_id
-# Description: Given a Channel with ID channel_id that the authorised user is part of, provide basic details about the channel
-#
+'''
+Function name: channel_details()
+Parameters: (token, channel_id)
+Return type: { name, owner_members, all_members }
+Exception: ValueError when:
+- Channel (based on ID) does not exist
+AccessError when:
+- Authorised user is not a member of channel with channel_id
+Description: Given a Channel with ID channel_id that the authorised user is
+part of, provide basic details about the channel
+'''
 
-from flask import Flask, request
 import json
-
-APP = Flask(__name__)
+import myexcept
 
 def getData():
     with open('export.json', 'r') as FILE:
@@ -21,13 +21,10 @@ def getData():
 # converting dictionary into string for flask
 def sendSuccess(data):
     return json.dumps(data)
-    
-@APP.route('/channel/details', methods = ['GET'])
-def channel_details(): 
-    data = getData()
-    token = request.args.get('token')
-    channel_id = request.args.get('channel_id')
 
+
+def channel_details(token, channel_id):
+    data = getData()
     #Test for valid token
     flag = 0
     for user in data['users']:
@@ -36,7 +33,7 @@ def channel_details():
             flag = 1
 
     if flag == 0:
-        raise ValueError("Session has expired. Please refresh the page and login\n")
+        myexcept.token_error()
 
     flag = 0
     flag2 = 0
@@ -54,11 +51,4 @@ def channel_details():
                     return sendSuccess(answer)
 
     if flag == 0 or flag2 == 0:
-        raise ValueError("Invalid channel or User is not a member of the channel")
-
-if __name__ == '__main__':
-    APP.run(port = 7878)
-
-# ValueError when:Channel ID is not a valid channel
-# AccessError whenAuthorised user is not a member of channel with channel_id
-
+        myexcept.member_not_in_channel()
