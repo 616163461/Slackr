5b94e1cdabdfbabbc611bad831ffaa9c55d776d4
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-27 01:55:41 +1100

    Update f_message_send.py

--- server/f_message_send.py
+++ server/f_message_send.py
@@ -1,16 +1,15 @@
-# Function name: message_send()
-# Parameters: (token, channel_id, message)
-# Return type: {}
-# Exception: ValueError when:
-# - Message is more than 1000 characters
-# Description: Send a message from authorised_user to the channel specified by channel_id
-#
-
-from flask import Flask, request
+'''
+Function name: message_send()
+Parameters: (token, channel_id, message)
+Return type: {}
+Exception: ValueError when:
+- Message is more than 1000 characters
+Description: Send a message from authorised_user to the channel specified by channel_id
+'''
 import json
 from random import randint
-# Have no implemented time
-APP = Flask(__name__)
+import myexcept
+import datetime
 
 def getData():
     with open('export.json', 'r') as FILE:
@@ -20,21 +19,17 @@ def getData():
 # converting dictionary into string for flask
 def sendSuccess(data):
     return json.dumps(data)
-    
+
 def updateData(data):
     with open('export.json', 'w') as FILE:
         json.dump(data, FILE)
     return 0
 
 
-@APP.route('/message/send', methods = ['POST'])    
-def message_send():
-    token = request.form.get('token')
-    channel_id = request.form.get('channel_id')
-    message = request.form.get('message')
+def message_send(token, channel_id, message):
     if len(message) > 1000:
-        raise ValueError('Message contains too many characters\n')
-    
+        myexcept.invalid_message()
+
     data_new = getData()
     flag = 0
     #Test for valid token
@@ -43,8 +38,8 @@ def message_send():
             u_id = i['u_id']
             flag = 1
     if flag == 0:
-        raise ValueError("Session has expired. Please refresh the page and login\n")
-    
+        myexcept.token_error()
+
     #Test if channel_id is valid and that user is in the channel_id
     channel_found = 0
     member_found = 0
@@ -60,23 +55,18 @@ def message_send():
                     answer['message_id'] = message_id
                     answer['u_id'] = u_id
                     answer['message'] = message
-                    answer['time_created'] = "12:04"
+                    answer['time_created'] = datetime.now()
                     answer['reacts'] = []
                     answer['is_pinned'] = False
-                    for i in range(0,50):
+                    for i in range(0, 50):
                         j['messages'].append(answer)
                     updateData(data_new)
                     return sendSuccess({'message_id' : message_id})
     if channel_found == 0:
-       raise ValueError("Channel not found...")
+        myexcept.channel_not_found()
     elif member_found == 0:
-       raise ValueError("Action could not be completed. User does not belong in the channel...\n") 
-                    
-@APP.route('/data', methods = ['GET'])    
+        myexcept.member_not_in_channel()
+
+@APP.route('/data', methods=['GET'])
 def getdataa():
     return sendSuccess(data)
-
-
-if __name__ == '__main__':
-    APP.run(port = 7878)
-
