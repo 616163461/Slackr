55277e109f75ef8d116233aa80cb57e168d05351
Geoffrey He <geoffrey.he@student.unsw.edu.au>
2019-10-27 11:19:27 +1100

    output tested

--- server/channel_details_test.py
+++ server/channel_details_test.py
@@ -13,8 +13,12 @@ from f_channel_details import channel_details
 from f_auth_register import auth_register
 from f_channels_create import channels_create
 from f_auth_logout import auth_logout
-
-
+import json
+# retrieve data from local data base 
+def getData():
+    with open('export.json', 'r') as FILE:
+        data = json.load(FILE)
+    return data
 
 def test_channel_details(): 
     
@@ -28,7 +32,12 @@ def test_channel_details():
     
     # SETUP END 
     
-    assert channel_details(token, channel_id) == {"name": "validchannel", "owner_members": [{"u_id": u_id, "name_first": "firstname", "name_last": "lastname"}], "all_members": [{}]}
+    # checking output of channel_details matches our data base
+    data = getData()
+    for channels in data['channels']:
+        if channels['channel_id'] == channel_id:
+            assert channel_details(token, channel_id) == channels
+    
     
     
 def test_channel_details_bad(): 
