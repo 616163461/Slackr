10ea0617e4c0d6d8e44354529656c36088b505a1
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-27 17:12:47 +1100

    Update f_admin_userpermission_change.py

--- server/f_admin_userpermission_change.py
+++ server/f_admin_userpermission_change.py
@@ -1,21 +1,19 @@
-# Function: admin_userpermission_change()
-# Parameters: (token, u_id, permission_id)
-# Output: {}
-# Exception: ValueError when:
-# - u_id does not refer to a valid user
-# - permission_id does not refer to a value permission
-# AccessError when:
-# - The authorised user is not an admin or owner
-# Description: Given a User by their user ID, set their permissions to new permissions described by permission_id
-# 
+'''
+Function: admin_userpermission_change()
+Parameters: (token, u_id, permission_id)
+Output: {}
+Exception: ValueError when:
+- u_id does not refer to a valid user
+- permission_id does not refer to a value permission
+AccessError when:
+- The authorised user is not an admin or owner
+Description: Given a User by their user ID, set their permissions
+to new permissions described by permission_id
+'''
 
-from flask import Flask, request
 from json import dumps
 import myexcept
 import json
-
-APP = Flask(__name__)
-
 
 def getData():
     with open('export.json', 'r') as FILE:
@@ -31,17 +29,14 @@ def updateData(data):
         json.dump(data, FILE)
     return 0
 
-@APP.route('/admin/userpermission/change', methods = ['POST'])
-def admin_userperm_change():
+
+def admin_userperm_change(token, u_id, permission_id):
     data = getData()
-    token = request.form.get('token')
-    permission_id = request.form.get('permission_id')
-    u_id = request.form.get('u_id')
-    
+
     #Test for valid token
     valid_token = False
     for user in data['users']:
-        if user['token'] == token and token != None: 
+        if user['token'] == token and token != None:
             perm_id = user['permission_id']
             valid_token = True
 
@@ -60,15 +55,7 @@ def admin_userperm_change():
             user_found = True
             user['permission_id'] = permission_id
             updateData(data)
-            return sendSuccess({})
+            return {}
 
     if user_found == False:
         myexcept.invalid_user()
-
-        
-if __name__ == '__main__':
-    APP.run(port = 5999)
-
-# Given a User by their user ID, 
-# set their permissions to new permissions described 
-# by permission_id
