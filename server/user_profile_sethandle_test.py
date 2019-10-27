4b226c435c05a7e457ed59d920b4e73fccd022a2
Richard Jiang <richard.jiang1@student.unsw.edu.au>
2019-10-27 12:03:59 +1100

    Update user_profile_sethandle_test.py

--- server/user_profile_sethandle_test.py
+++ server/user_profile_sethandle_test.py
@@ -10,13 +10,6 @@ import pytest
 from f_user_profile_sethandle import user_profile_sethandle
 from f_auth_register import auth_register
 from f_auth_logout import auth_logout
-import json
-
-# retrieve data from local data base 
-def getData():
-    with open('export.json', 'r') as FILE:
-        data = json.load(FILE)
-    return data
 
 def test_user_profile_sethandle():
     # SETUP BEGIN
@@ -25,8 +18,8 @@ def test_user_profile_sethandle():
     handle_str_max = "hithisis20characters"
     
     validAuthRegisterDic = auth_register("valid@email.com", "validpassword", "Richard", "Jiang")
-    token = authRegisterDic['token']
-    u_id = authRegisterDic['u_id']
+    token = validAuthRegisterDic['token']
+    u_id = validAuthRegisterDic['u_id']
     
     invalidAuthRegisterDicTwo = auth_register("valid2@email.com", "validpassword", "firstname", "lastname")
     invalid_token = invalidAuthRegisterDicTwo['token']
@@ -37,31 +30,8 @@ def test_user_profile_sethandle():
     
     # Testing successful run (default case)
     assert user_profile_sethandle(token, handle_str_good) == {}
-    
-    # checking name is changed in local data base
-    handle_set = False
-    data = getData()
-    for users in data['users']:
-        if users['token'] == token:
-            if users['handle_str'] == handle_str_good:
-                handle_set = True
-
-    if handle_set == False: 
-        raise ValueError(f"Handle set unsuccessful...\n")
-        
     # Testing maximum case of handle_str
     assert user_profile_sethandle(token, handle_str_max) == {}
-    
-    # checking name is changed in local data base
-    handle_set = False
-    data = getData()
-    for users in data['users']:
-        if users['token'] == token:
-            if users['handle_str'] == handle_str_max:
-                handle_set = True
-
-    if handle_set == False: 
-        raise ValueError(f"Handle set unsuccessful...\n")
     
     with pytest.raises(ValueError):
         # Testing good token, with bad handle_str
@@ -70,8 +40,3 @@ def test_user_profile_sethandle():
         user_profile_sethandle(invalid_token, handle_str_good)
         # Testing both bad token and bad handle_str
         user_profile_sethandle(invalid_token, handle_str_bad)
-
-        
-        
-    
-
